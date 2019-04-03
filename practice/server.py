cc59c75abca4949c40d9bb6fb2f19937e3794ac2
epgomez <eloasasarus@gmail.com>
2019-04-03 19:14:57 +0200

    3-3-2019 basic level done, need to check exceptions

--- practice/server.py
+++ practice/server.py
@@ -1,62 +1,124 @@
 import socketserver, http.server, termcolor, requests
 
-PORT_server = 8050
+PORT_server = 8000
 server = 'http://rest.ensembl.org'
-ENDPOINTS = ['/info/species?']
+ENDPOINTS = ['/info/species?', '/info/assembly']
 headers = {"Content-Type": "application/json"}
 socketserver.TCPServer.allow_reuse_address = True
 
-html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">    <title>{}</title></head><body><h3>{}</h3><pre>{}</pre><a href="/">Main page</a></body></html>'
+# this is the future html file that will be sent to the client
+# It has blank spaces that will be filled later
+html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>{}</title></head><body><h3>{}</h3><a href="/">Main page</a><pre>{}</pre><a href="/">Main page</a></body></html>'
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        global base_op, length
         termcolor.cprint(self.requestline, 'green')
         path = self.path
 
         if path == '/':
+            # This is the first html file that is sent to the client
             with open('home.html', 'r') as f:
                 info = f.read()
                 resp = 200
 
         elif 'listSpecies' in path:
+
             resp = 200
             ext = ENDPOINTS[0]
+            # Process the client's request using the request module
             r = requests.get(server + ext, headers=headers)
-
             decoded = r.json()
+
+            # Limit to the length of the list selected by the user
+            limit = path.split('=')[1]
+
+            # If there is no limit, the limit used will be the species' list's length
+            if limit == '':
+                limit = len(decoded['species'])
             add = ''
-            for i in range(len(decoded['species'])):
+
+            # Add the common and the scientific name of all the species to the list
+            for i in range(int(limit)):
                 add += 'Scientific name: {} \nCommon name: {}\n\n'.format(decoded['species'][i]['name'], decoded['species'][i]['common_name'])
-            print(add)
-            tytle = 'Species list'
+
+            # The title of the html file is different in each case
+            title = 'Species list'
             h = 'List of all the available species'
-            info = html.format(tytle, h, add)
+
+            # Include the information in the future html response text
+            info = html.format(title, h, add)
+
+        elif 'karyotype' in path:
+
+            resp = 200
+            specie = path.split('=')[1]
+            ext = ENDPOINTS[1] + '/' + specie + '?'
+            r = requests.get(server + ext, headers=headers)
+            decoded = r.json()
+
+            # In case that the specie's name is not on the database the client will recieve a response message indicating so
+            if r.ok:
+                add = ''
+                if decoded['karyotype'] == []:
+                    add = "This specie's karyotype is not stored in the database"
+                else:
+                    # Add all the chromosomes to the list, avoiding the one called "MT", which is a parameter used by the database itself
+                    for i, elem in enumerate(decoded['karyotype']):
+                        if elem == 'MT':
+                            continue
+                        add += 'Chromosome number {}: {}\n'.format(str(i+1), elem)
+            else:
+                add = 'Can not find internal name for species "{}"'.format(specie)
+
+            # Like before, I include the title myself
+            title = 'Karyotype'
+            h = 'Karyotype of {}'.format(path.split('=')[1])
+            info = html.format(title, h, add)
+
+        elif 'chromosomeLength' in path:
+
+            resp = 200
+            # In this case, I receive two mandatory endpoints: "specie" and "chromo"
+            specie = path.split('&')[0].split('=')[1]
+            chromo = path.split('&')[1].split('=')[1]
+            # Process the request
+            ext = ENDPOINTS[1] + '/'+ specie + '/'+ chromo + '?'
+            r = requests.get(server + ext , headers=headers)
+            decoded = r.json()
+
+            if r.ok:
+                add = decoded['length']
+            else:
+                add = 'Can not find chromosome {} of specie {}'.format(chromo,specie)
+
+            # Include the information in my future html file
+            title = 'Chromosome lenght'
+            h = 'Lenght of the {} chromosome of {}'.format(chromo, specie)
+            info = html.format(title, h, add)
 
         else:
+            # In the case that I get an endpoint different from the ones I have decided to use, the client recieves an error message
             resp = 404
             f = open('error.html', 'r')
-            content = f.read()
+            info = f.read()
 
+        # Write the information in a response html file
+        d = open('response.html', 'w')
+        d.write(info)
+        d.close()
 
-        if resp == 404:
-            self.send_response(resp)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(str.encode(content)))
-            self.end_headers()
+        # Read the information form that response html file
+        d = open('response.html', 'r')
+        content = d.read()
+        d.close()
 
-        else:
-            d = open('response.html', 'w')
-            d.write(info)
-            d.close()
-            d = open('response.html', 'r')
-            content = d.read()
-            self.send_response(resp)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(str.encode(content)))
-            self.end_headers()
+        # Send the headers and the response html
+        self.send_response(resp)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(content)))
+        self.end_headers()
 
         self.wfile.write(str.encode(content))
 
--- practice/try.py
+++ practice/try.py
@@ -1,3 +1,22 @@
-b = 'adsdd{} asdhnuibvuib,{}'
-b = b.format('dhfgudfg', 'hey')
-print(b)
+import socketserver, http.server, termcolor, requests,sys
+
+PORT_server = 8050
+server = 'http://rest.ensembl.org'
+ENDPOINTS = ['/info/species?', '/info/assembly']
+headers = {"Content-Type": "application/json"}
+socketserver.TCPServer.allow_reuse_address = True
+
+
+specie = '/' + input('enter: ')
+chromo = '/' + input('chr: ')
+ext = ENDPOINTS[1] + specie + chromo +'?'
+print(server + ext + specie)
+
+r = requests.get(server + ext, headers=headers)
+
+if not r.ok:
+    r.raise_for_status()
+    sys.exit()
+
+decoded = r.json()
+print(decoded)
